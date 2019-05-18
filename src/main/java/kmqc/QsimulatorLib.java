39c3be43c412fac0cfcdf9b4be2b78c162d8afda
Rustam Sayfutdinov <rstm.sf@gmail.com>
2019-05-18 19:16:37 +0300

    Добавлены обертки на инструкции

--- src/main/java/kmqc/QsimulatorLib.java
+++ src/main/java/kmqc/QsimulatorLib.java
@@ -51,15 +51,60 @@ public class QsimulatorLib {
 
     public native void measure(
       @Cast("uint64_t") long idx_qreg, @Cast("uint64_t") long idx_creg);
-      
+
     public native @Cast("uint32_t") int get_creg(@Cast("uint64_t") long idx_creg);
+
+    public native void applyX(
+      @Cast("uint64_t") long idx_qreg, @Cast("uint64_t") long i, double x, double y);
+
+    public native void applyZ(
+      @Cast("uint64_t") long idx_qreg, @Cast("uint64_t") long i, double tau);
+
+    public native void applyXconjugate(
+      @Cast("uint64_t") long idx_qreg, @Cast("uint64_t") long i, double x, double y);
+
+    public native void applyZconjugate(
+      @Cast("uint64_t") long idx_qreg, @Cast("uint64_t") long i, double tau);
   }
 
 
   public static void main(String[] args) {
+    test3();
+  }
+
+  private static void test1() {
     Simulator qsim = new Simulator(1, 2);
     qsim.measure(0, 0);
     System.out.println("vector result: " + qsim.get_creg(0));
+
+    try {
+      qsim.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void test2() {
+    Simulator qsim = new Simulator(1, 2);
+    double half = 1.0 / 2.0;
+    qsim.applyX(
+      0, 1, java.lang.Math.sqrt(half), java.lang.Math.sqrt(half));
+    qsim.measure(0, 0);
+    System.out.println("result 0: " + qsim.get_creg(0));
+
+    try {
+      qsim.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  
+  private static void test3() {
+    Simulator qsim = new Simulator(1, 2);
+    double half = 1.0 / 2.0;
+    qsim.applyZ(0, 1, 0.0);
+    qsim.measure(0, 0);
+    System.out.println("result 0: " + qsim.get_creg(0));
 
     try {
       qsim.close();
